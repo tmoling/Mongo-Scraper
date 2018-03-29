f844abbbee93e56867f542c3628be7ed3ddd1f14
Troy Moling <troy.moling@gmail.com>
2018-03-29 14:56:12 -0500

    more port changes

--- server.js
+++ server.js
@@ -154,7 +154,6 @@ router.post('/articles/comment/:id2/delete', function (req, res) {
 
 var port = process.env.PORT || 3000;
 
-var port = 3000;
 app.listen(port, function () {
   console.log("App running on port " + port + "!");
 });
